8f74c4504131cd6d395302b933eb034cadad7c3d
Максим Маркелов <M.Markelov@iepi.ru>
2023-04-18 20:17:28 +0300

    remove Failtest

--- src/test/java/ru/netology/steps/TransferSteps.java
+++ src/test/java/ru/netology/steps/TransferSteps.java
@@ -18,31 +18,23 @@ public class TransferSteps {
 
     @Пусть("пользователь залогинен с именем «vasya» и паролем «qwerty123»")
     public void loginWithNameAndPassword() {
-        System.out.println("loginWithNameAndPassword");
         // Write code here that turns the phrase above into concrete actions
         open("http://localhost:9999");
-        System.out.println("opened");
         dashboardPage = loginPage
                 .validLogin(DataHelper.getAuthInfo())
                 .validVerify(DataHelper.getVerificationCodeFor(DataHelper.getAuthInfo()));
-
-//        throw new io.cucumber.java.PendingException();
     }
 
 
     @Когда("пользователь переводит {int} рублей с карты с номером {string} на свою {int} карту с главной страницы")
     public void transferExistedMoneyAmountCardWithNumberToCardWithIndex(Integer amount, String cardNumber, Integer cardIndex) {
-        System.out.println("transferExistedMoneyAmountCardWithNumberToCardWithIndex");
         // Write code here that turns the phrase above into concrete actions
         dashboardPage.DepositFromCardWithNumberToCardWithIndex(amount, cardNumber, cardIndex);
-//        throw new io.cucumber.java.PendingException();
     }
     @Тогда("баланс его {int} карты из списка на главной странице должен стать {int} рублей")
     public void checkCardBalanceByCardIndex(Integer cardIndex, Integer balance) {
-        System.out.println("checkCardBalanceByCardIndex");
         // Write code here that turns the phrase above into concrete actions
         assertEquals(balance, dashboardPage.getCardBalanceByIndex(cardIndex - 1));
-//        throw new io.cucumber.java.PendingException();
     }
 
 }
